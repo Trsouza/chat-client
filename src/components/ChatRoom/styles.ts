00df93198dafc6cf55c128574ed505fb9d7af111
trsouza <tathianesouza1@gmail.com>
2023-07-09 18:39:15 -0300

    Style adjustment

--- src/components/ChatRoom/styles.ts
+++ src/components/ChatRoom/styles.ts
@@ -5,35 +5,33 @@ export const Container = styled.div`
 `;
 
 export const Title = styled.h1`
-  color:  #ed4b05;
+  color: #ed4b05;
   display: flex;
   justify-content: center;
-  padding: 10px;
-  letter-spacing: 2px;
 `;
 
 export const Button = styled.button`
   border: none;
   padding: 10px;
   background: #ed4b05;
-  color:#fff;
+  color: #fff;
   font-size: 1.2em;
   font-weight: bold;
 `;
 
 export const Register = styled.div`
   position: fixed;
-  padding:30px;
-  box-shadow:0 2.8px 2.2px rgba(249, 165, 86, 0.034),0 6.7px 5.3px rgba(249, 165, 86, 0.034),0 12.5px 10px rgba(249, 165, 86, 0.034),0 22.3px 17.9px rgba(249, 165, 86, 0.034),0 41.8px 33.4px rgba(249, 165, 86, 0.034),0 100px 80px rgba(249, 165, 86, 0.034);
-  top:40%;
-  left:40%;
+  padding: 30px;
+  box-shadow: 0 2.8px 2.2px rgba(249, 165, 86, 0.034),0 6.7px 5.3px rgba(249, 165, 86, 0.034),0 12.5px 10px rgba(249, 165, 86, 0.034),0 22.3px 17.9px rgba(249, 165, 86, 0.034),0 41.8px 33.4px rgba(249, 165, 86, 0.034),0 100px 80px rgba(249, 165, 86, 0.034);
+  top: 40%;
+  left: 40% ;
   display: flex;
   flex-direction: row;
 `;
 
 export const ChatBox = styled.div`
-  box-shadow:0 2.8px 2.2px rgba(249, 165, 86, 0.034),0 6.7px 5.3px rgba(249, 165, 86, 0.034),0 12.5px 10px rgba(249, 165, 86, 0.034),0 22.3px 17.9px rgba(249, 165, 86, 0.034),0 41.8px 33.4px rgba(249, 165, 86, 0.034),0 100px 80px rgba(249, 165, 86, 0.034);
-  margin:40px 50px;
+  box-shadow: 0 2.8px 2.2px rgba(249, 165, 86, 0.034),0 6.7px 5.3px rgba(249, 165, 86, 0.034),0 12.5px 10px rgba(249, 165, 86, 0.034),0 22.3px 17.9px rgba(249, 165, 86, 0.034),0 41.8px 33.4px rgba(249, 165, 86, 0.034),0 100px 80px rgba(249, 165, 86, 0.034);
+  margin: 40px 50px;
   height: 600px;
   padding: 10px;
   display: flex;
@@ -45,7 +43,7 @@ export const MemberList = styled.div`
 `;
 
 export const ChatContent = styled.div`
-  width:80%;
+  width: 80%;
   margin-left: 10px;
 `;
 
@@ -64,7 +62,7 @@ export const ChatMessages = styled.ul`
     background-color: #adff2f
   }
   .message {
-    padding:5px;
+    padding: 5px;
     width: auto;
     display: flex;
     flex-direction: row;
